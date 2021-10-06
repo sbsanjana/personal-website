7567d237c813e829deaa9d49f28b8b9ec9b26921
“Sanjana <sbsanjana@gmail.com>
2021-10-06 15:26:20 -0400

    updated favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,6 @@ module.exports = {
       options: {
         fonts: [
            `Roboto Mono`,`Roboto`,`Staatliches`
-          
         ],
       },
     },
@@ -27,13 +26,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Sanjana Badhya`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/new_fav.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
